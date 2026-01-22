335cd750f38d77fd0d20d969ae993a9a5eb968f7
Your Name <you@example.com>
2026-01-22 22:12:08 +0530

    Initial commit - responsive header

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,7 @@
 console.log("E-Commerce Website Loaded");
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
